3d5290248886cb0a427b05bc44dc6afc9e30a1cc
b-dur <b-dur@users.noreply.github.com>
2020-03-15 17:19:23 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.1a10697d78507c62e2616205d1ae79a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632227487c54c688db91d3d09599bf7f",
+    "url": "/ellada/index.html"
+  },
+  {
+    "revision": "8ffe50787e3f06249c4a",
+    "url": "/ellada/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ebb91c0e3155323c7099",
+    "url": "/ellada/static/js/2.a40561b6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ellada/static/js/2.a40561b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ffe50787e3f06249c4a",
+    "url": "/ellada/static/js/main.f986b7e6.chunk.js"
+  },
+  {
+    "revision": "680b9c6af4a2240ec43f",
+    "url": "/ellada/static/js/runtime-main.5ec578ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ellada/static/media/logo.5d5d9eef.svg"
+  }
+]);
